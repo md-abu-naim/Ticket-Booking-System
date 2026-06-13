7aa05f8346eaf7188dd71d7b793fa686bcc3cb45
abunaim34 <farhanadnanfarabi@gmail.com>
2026-06-13 22:28:16 +0600

    create mathes table

--- SQL Queries.sql
+++ SQL Queries.sql
@@ -1,6 +1,4 @@
--- =========================================================================
 -- 1. CREATE USERS TABLE
--- =========================================================================
 CREATE TABLE Users (
     user_id serial PRIMARY KEY,
     full_name VARCHAR(200) UNIQUE,
@@ -9,12 +7,22 @@ CREATE TABLE Users (
     phone_number VARCHAR(50)
 );
 
--- =========================================================================
 -- DATA SEEDING: INSERT SAMPLE DATA INTO USERS
--- =========================================================================
 INSERT INTO Users (user_id, full_name, email, role, phone_number) VALUES
 (1, 'Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111'),
 (2, 'Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'),
 (3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
 (4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
 
+
+-- 2. CREATE MATCHES TABLE
+CREATE TABLE Matches (
+    match_id SERIAL PRIMARY KEY,
+    fixture VARCHAR(255),
+    tournament_category VARCHAR(100),
+    base_ticket_price DECIMAL(10, 2)
+    CHECK(base_ticket_price > 0),
+  
+    match_status VARCHAR(50)
+    CHECK(match_status IN('Available', 'Selling Fast', 'Sold Out', 'Postponed'))
+);
