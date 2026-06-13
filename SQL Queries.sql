d8247b7078995692a2aa9ec3c652466a7ca9c451
abunaim34 <farhanadnanfarabi@gmail.com>
2026-06-13 22:29:16 +0600

    insert data to Matches table

--- SQL Queries.sql
+++ SQL Queries.sql
@@ -26,3 +26,12 @@ CREATE TABLE Matches (
     match_status VARCHAR(50)
     CHECK(match_status IN('Available', 'Selling Fast', 'Sold Out', 'Postponed'))
 );
+
+
+-- DATA SEEDING: INSERT SAMPLE DATA INTO MATCHES
+INSERT INTO Matches (match_id, fixture, tournament_category, base_ticket_price, match_status)
+VALUES(10, 'Real Madrid vs Barcelona', 'Champions League', 20, 'Available'),
+(102, 'Man City vs Liverpool', 'Premier League', 120.00, 'Selling Fast'),
+(103, 'Bayern Munich vs PSG', 'Champions League', 130.00, 'Available'),
+(104, 'AC Milan vs Inter Milan', 'Serie A', 90.00, 'Sold Out'),
+(105, 'Juventus vs Roma', 'Serie A', 80.00, 'Available');
